bc05236248d76ce1676025747de6214ddbefb41a
Zack Kollar <rallokkcaz@gmail.com>
2022-02-27 03:02:44 -0800

    :notes: Experiments... EXPERIMENTS

--- src/capteur-cli/src/main.rs
+++ src/capteur-cli/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+//!
+//! The command line interface to emit messages coming from the serial/UART update ticks.
+//! 
+//! **Supported transport layers:**
+//! 
+//! - [tokio-amqp](https://crates.io/crates/tokio-amqp)
+//! - *TBD: Other transport layers yet TBD!*
+//! 
+
+use color_eyre::Report;
+
+use capteur::setup_environment;
+
+#[tokio::main]
+async fn main() -> Result<(), Report> {
+    setup_environment()?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
+
---
+++ src/capteur/src/lib.rs
@@ -0,0 +1,37 @@
+//!
+//! The Capteur platform and building blocks
+//! 
+
+use color_eyre::Report;
+use tracing_subscriber::EnvFilter;
+
+
+/// 
+/// Setup tracing and error handling with [`tracing`], [`tracing_subscriber`] and [`color_eyre`].
+/// 
+pub fn setup_environment() -> Result<(), Report> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "1")
+    }
+
+    color_eyre::install()?;
+
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info")
+    }
+    
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
