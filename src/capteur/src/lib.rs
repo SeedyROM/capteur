ac707752c5ff8afdb91be5137e7a25f6417c1e18
Zack Kollar <rallokkcaz@gmail.com>
2022-02-27 03:41:44 -0800

    :notes: We've got apps

--- src/capteur/src/lib.rs
+++ src/capteur/src/lib.rs
@@ -1,15 +1,15 @@
 //!
 //! The Capteur platform and building blocks
-//! 
+//!
 
 use color_eyre::Report;
 use tracing_subscriber::EnvFilter;
 
 pub mod transports;
 
-/// 
+///
 /// Setup tracing and error handling with [`tracing`], [`tracing_subscriber`] and [`color_eyre`].
-/// 
+///
 pub fn setup_environment() -> Result<(), Report> {
     if std::env::var("RUST_LIB_BACKTRACE").is_err() {
         std::env::set_var("RUST_LIB_BACKTRACE", "1")
@@ -20,11 +20,10 @@ pub fn setup_environment() -> Result<(), Report> {
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", "info")
     }
-    
+
     tracing_subscriber::fmt::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
     Ok(())
 }
-
