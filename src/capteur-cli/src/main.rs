60e42b33f50ac4a3fef53bb6a5cc2bafefdfae85
Zack Kollar <rallokkcaz@gmail.com>
2022-02-27 17:15:57 -0800

    :hammer: Add WebSocketPassthrough and focus on AMQP for now

--- src/capteur-cli/src/main.rs
+++ src/capteur-cli/src/main.rs
@@ -12,6 +12,7 @@ use color_eyre::Report;
 use capteur::{
     setup_environment,
     transports::{Transport, AMQP},
+    websockets::WebSocketPassthrough,
 };
 use tracing::info;
 
@@ -23,8 +24,17 @@ async fn main() -> Result<(), Report> {
     info!("Let's emit some garbage!!!");
 
     let mut transport = AMQP::new().await?;
+    let mut ws_passthrough = WebSocketPassthrough::from_channel(transport.channel.clone());
 
-    let _ = tokio::task::spawn(async move { transport.stream().await }).await?;
+    let amqp_transport = tokio::task::spawn(async move { transport.stream().await });
+    let amqp_passthrough = tokio::task::spawn(async move { ws_passthrough.stream().await });
+
+    tokio::select! {
+        _ = amqp_transport => (),
+        _ = amqp_passthrough => (),
+    };
+
+    info!("Shutting down...");
 
     Ok(())
 }
